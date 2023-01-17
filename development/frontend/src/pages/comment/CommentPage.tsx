4818b0556b866b4bc3ebc291f3e4ab41f1f0c782
ngaipo <poo18h@gmail.com>
2023-01-17 18:14:46 +0800

    update jason filter

--- development/frontend/src/pages/comment/CommentPage.tsx
+++ development/frontend/src/pages/comment/CommentPage.tsx
@@ -1,13 +1,79 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function CommentPage() {
-    const comment = [
-        {id:1, title: 'good', content: 'nice'},
-        {id:2, title:'bad', content: 'bad'},
-    ]
+  const comments = [
+    {
+      id: 1,
+      title: "good",
+      service: "baby shop",
+      username: "tom",
+      is_thumb: true,
+    },
+    {
+      id: 2,
+      title: "bad",
+      service: "food shop",
+      username: "peter",
+      is_thumb: false,
+    },
+    {
+      id: 3,
+      title: "bad 123",
+      service: "food shop",
+      username: "peter 123",
+      is_thumb: false,
+    },
+  ];
+
+  const [filter, setFilter] = useState<{
+    is_thumb?: boolean;
+    title?: string;
+  }>({});
+  const filteredComments = comments.filter((comment) => {
+    let isFiltered = true;
+    for (let key in filter) {
+      // @ts-ignore
+      if (comment[key] !== filter[key]) {
+        isFiltered = false;
+        break;
+      }
+    }
+    return isFiltered;
+  });
+
   return (
-    <div>CommentPage</div>
-  )
+    <div>
+      CommentPage
+      <div>
+        <button>篩選</button>
+        <button>排序</button>
+        <button
+          onClick={() => setFilter((filter) => ({ ...filter, title: "bad" }))}
+        >
+          好評
+        </button>
+        <button
+          onClick={() => setFilter((filter) => ({ ...filter, is_thumb: true }))}
+        >
+          好評
+        </button>
+        <button
+          onClick={() =>
+            setFilter((filter) => ({ ...filter, is_thumb: false }))
+          }
+        >
+          差評
+        </button>
+      </div>
+      {filteredComments.map((comment) => (
+        <div key={comment.id}>
+          <p>{comment.title}</p>
+          <p>{comment.service}</p>
+          <p>{comment.username}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default CommentPage
+export default CommentPage;

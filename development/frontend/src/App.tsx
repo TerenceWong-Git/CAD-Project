2db0e9d8ec4ffa7794b20705a32c07f2b54bca5c
Terence <haha@example.com>
2023-01-17 19:21:07 +0800

    Create Login page

--- development/frontend/src/App.tsx
+++ development/frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router";
-import Map from "./pages/Map";
+import Login from "./pages/Login";
+import Map from "./pages/Map/Map";
+import { Link } from "react-router-dom";
+import CommentPage from "./pages/comment/CommentPage";
 
 function App() {
   return (
     <div className="App">
+      <Link to={"/login"}>Login</Link>
       <Routes>
+        <Route path="/login" element={<Login />}></Route>
         <Route path="/map" element={<Map />}></Route>
         <Route path="/comments" element={<CommentPage />}></Route>
       </Routes>

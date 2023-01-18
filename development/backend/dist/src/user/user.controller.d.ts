07986f660f77e4d3ee28b5ae2120d3da29ed1ec7
Happyhobby11 <jeffreypang302@gmail.com>
2023-01-18 17:08:13 +0800

    remove dist folder

--- development/backend/dist/src/user/user.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { RegisterDto } from './dto';
-import { UserService } from './user.service';
-export declare class UserController {
-    private readonly userService;
-    constructor(userService: UserService);
-    getSelfInfo(userId: number): Promise<"string" | "number" | "bigint" | "boolean" | "symbol" | "undefined" | "object" | "function">;
-    register(registerDto: RegisterDto): Promise<void>;
-}
--- development/backend/dist/src/user/user.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaService } from 'src/prisma/prisma.service';
-import { RegisterDto } from './dto';
-export declare class UserService {
-    private readonly prismaService;
-    constructor(prismaService: PrismaService);
-    getSelfInfo(userId: number): Promise<"string" | "number" | "bigint" | "boolean" | "symbol" | "undefined" | "object" | "function">;
-    register(registerDto: RegisterDto): Promise<void>;
-}

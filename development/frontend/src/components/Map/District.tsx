c5f81893af712a41798e63b412d84b9a6bfe944b
Terence <haha@example.com>
2023-01-26 18:21:22 +0800

    Update deploy setting

--- development/frontend/src/components/Map/District.tsx
+++ development/frontend/src/components/Map/District.tsx
@@ -19,25 +19,26 @@ const districts = [
   "Wan Chai",
 ];
 
-const Type = [
-  "Islands",
-  "Kwai_tsing",
-  "North",
-  "Sai_kung",
-  "Sha_tin",
-  "Tai_po",
-  "Tsuen_wan",
-  "Tuen_mun",
-  "Yuen_long",
-  "Kowloon_city",
-  "Kwun_tong",
-  "Sham_shui_po",
-  "Wong_tai_sin",
-  "Yau_tsim_mong",
-  "Central_and_western",
-  "Eastern",
-  "Southern",
-  "Wan_chai",
-];
+// const Type = [
+//   "Islands",
+//   "Kwai_tsing",
+//   "North",
+//   "Sai_kung",
+//   "Sha_tin",
+//   "Tai_po",
+//   "Tsuen_wan",
+//   "Tuen_mun",
+//   "Yuen_long",
+//   "Kowloon_city",
+//   "Kwun_tong",
+//   "Sham_shui_po",
+//   "Wong_tai_sin",
+//   "Yau_tsim_mong",
+//   "Central_and_western",
+//   "Eastern",
+//   "Southern",
+//   "Wan_chai",
+// ];
 
-export { districts, Type };
+// export { districts, Type };
+export default districts;

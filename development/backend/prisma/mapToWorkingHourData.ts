725cf78b470f154a500e2f35ea00d3aac0498fc4
Terence <haha@example.com>
2023-02-01 15:02:34 +0800

    Update seed data

--- development/backend/prisma/mapToWorkingHourData.ts
+++ development/backend/prisma/mapToWorkingHourData.ts
@@ -1,6 +1,16 @@
 const mapToWorkingHour = [
-  {
-    mapId: 1,
-    workingHourId: 1,
-  },
+  //   {
+  //     mapId: mapResId[1].id,
+  //     workingHourId: workingHourResId[0].id,
+  //   },
+  //   {
+  //     mapId: mapResId[1].id,
+  //     workingHourId: workingHourResId[0].id,
+  //   },
+  //   {
+  //     mapId: mapResId[1].id,
+  //     workingHourId: workingHourResId[0].id,
+  //   },
 ];
+
+export default mapToWorkingHour;

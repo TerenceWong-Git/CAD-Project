f6e975cff438520f2fa27df23239ce98c42c963f
Terence <haha@example.com>
2023-02-04 18:45:42 +0800

    Fix error

--- development/frontend/src/components/Header.tsx
+++ development/frontend/src/components/Header.tsx
@@ -1,11 +1,14 @@
 import "./Header.css";
 
 export default function Header() {
+  const logout = () => {
+    localStorage.removeItem("token");
+  };
   return (
     <>
       <div className="header">Name</div>
       <div className="logOut">
-        <button type="button" onClick={onImageRemoveAll}>
+        <button type="button" onClick={logout}>
           Logout
         </button>
       </div>

408965b53979962eeedfbb9a618a54ca579e1497
Terence <haha@example.com>
2023-02-05 21:03:23 +0800

    Update landing page css

--- development/frontend/src/pages/LandingPage.tsx
+++ development/frontend/src/pages/LandingPage.tsx
@@ -9,12 +9,17 @@ import { tags } from "../components/pet/PetTag";
 import "./LandingPage.css";
 
 export default function LandingPage() {
+  const breakpointColumnsObj = {
+    default: 3,
+    820: 2,
+    290: 1,
+  };
+
   const [allPicture, setAllPicture] = useState<any>([]);
   const [initialPicture, setInitialPicture] = useState<any>([]);
 
-  const [hasMore, setHasMore] = useState(true);
-
-  const [indexA, setIndexA] = useState(8);
+  const [initialHasMore, setInitialHasMore] = useState(true);
+  const [indexA, setIndexA] = useState(9);
 
   useEffect(() => {
     async function getLanding() {
@@ -22,30 +27,38 @@ export default function LandingPage() {
       const json = await res.json();
 
       setAllPicture(json);
-      setInitialPicture(json.slice(0, 8));
+      setInitialPicture(json.slice(0, 9));
     }
     getLanding();
   }, []);
 
-  const fetchData = async () => {
-    setInitialPicture([...initialPicture, allPicture[indexA]]);
-    if (allPicture.length - initialPicture.length < 2) {
-      setHasMore(false);
+  const notYetFilteredLandingPicture = async () => {
+    if (!isFiltered) {
+      setInitialPicture([...initialPicture, allPicture[indexA]]);
+      if (allPicture.length - initialPicture.length < 2) {
+        setInitialHasMore(false);
+      }
+      setIndexA(indexA + 1);
     }
-    setIndexA(indexA + 1);
-  };
-
-  const breakpointColumnsObj = {
-    default: 3,
-    820: 2,
-    290: 1,
   };
 
   /////////////////////////////////////   Category   /////////////////////////////////////
 
   const [filteredPicture, setFilteredPicture] = useState<any>([]);
+  const [isShownFilteredPicture, setIsShownFilteredPicture] = useState<any[]>([]);
+
+  const [hasMore, setHasMore] = useState(true);
+  const [indexB, setIndexB] = useState(9);
+  const [isFiltered, setIsFiltered] = useState(false);
+
+  function clearTag() {
+    window.scrollTo(0, 0);
+    setIsFiltered(false);
+  }
 
   function getTag(tag: any) {
+    window.scrollTo(0, 0);
+    setIsFiltered(true);
     landingTagFilter(tag);
   }
 
@@ -58,11 +71,28 @@ export default function LandingPage() {
     });
 
     setFilteredPicture(newItems);
+    setIsShownFilteredPicture(newItems.slice(0, 9));
   };
 
-  console.log(filteredPicture);
+  const filteredLandingPicture = async () => {
+    if (isFiltered) {
+      setIsShownFilteredPicture([...isShownFilteredPicture, filteredPicture[indexB]]);
+      if (filteredPicture.length - isShownFilteredPicture.length < 2) {
+        setHasMore(false);
+      }
+      setIndexB(indexB + 1);
+    }
+  };
 
   /////////////////////////////////////   Category   /////////////////////////////////////
+
+  console.log("拎左幾多野", allPicture);
+  console.log("一開頭show左幾多野", initialPicture);
+  console.log("==============");
+  console.log("簡左tag未", isFiltered);
+  console.log("簡完tag之後有幾多野", filteredPicture);
+  console.log("簡完tag之後show左幾多野", isShownFilteredPicture);
+  console.log("");
 
   return (
     <>
@@ -71,6 +101,9 @@ export default function LandingPage() {
 
         <div className="landingPageInfiniteScroll">
           <div className="landingPageTags">
+            <div>
+              <button onClick={() => clearTag()}>All</button>
+            </div>
             {tags.map((tag) => {
               return (
                 <div key={tag.value}>
@@ -79,27 +112,51 @@ export default function LandingPage() {
               );
             })}
           </div>
-          <InfiniteScroll
-            dataLength={initialPicture.length}
-            next={fetchData}
-            hasMore={hasMore}
-            loader={<h4>Loading...</h4>}
-            endMessage={
-              <p style={{ textAlign: "center" }}>
-                <b>Yay! You have seen it all</b>
-              </p>
-            }
-          >
-            <Masonry breakpointCols={breakpointColumnsObj} className="my-masonry-grid" columnClassName="my-masonry-grid_column">
-              {initialPicture.map((item: any) => {
-                return (
-                  <div key={item.name}>
-                    <img src={`${process.env.REACT_APP_S3_UPLOAD_URL}/${item.name}`} alt={item.name} />
-                  </div>
-                );
-              })}
-            </Masonry>
-          </InfiniteScroll>
+          {isFiltered ? (
+            <InfiniteScroll
+              dataLength={isShownFilteredPicture.length}
+              next={filteredLandingPicture}
+              hasMore={hasMore}
+              loader={<h4>Loading...</h4>}
+              endMessage={
+                <p style={{ textAlign: "center" }}>
+                  <b>Yay! You have seen it all</b>
+                </p>
+              }
+            >
+              <Masonry breakpointCols={breakpointColumnsObj} className="my-masonry-grid" columnClassName="my-masonry-grid_column">
+                {isShownFilteredPicture.map((item: any) => {
+                  return (
+                    <div key={item.name}>
+                      <img src={`${process.env.REACT_APP_S3_UPLOAD_URL}/${item.name}`} alt={item.name} />
+                    </div>
+                  );
+                })}
+              </Masonry>
+            </InfiniteScroll>
+          ) : (
+            <InfiniteScroll
+              dataLength={initialPicture.length}
+              next={notYetFilteredLandingPicture}
+              hasMore={initialHasMore}
+              loader={<h4>Loading...</h4>}
+              endMessage={
+                <p style={{ textAlign: "center" }}>
+                  <b>Yay! You have seen it all</b>
+                </p>
+              }
+            >
+              <Masonry breakpointCols={breakpointColumnsObj} className="my-masonry-grid" columnClassName="my-masonry-grid_column">
+                {initialPicture.map((item: any) => {
+                  return (
+                    <div key={item.name}>
+                      <img src={`${process.env.REACT_APP_S3_UPLOAD_URL}/${item.name}`} alt={item.name} />
+                    </div>
+                  );
+                })}
+              </Masonry>
+            </InfiniteScroll>
+          )}
         </div>
 
         <Footer />

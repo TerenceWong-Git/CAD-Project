74c7507b91924715142731c97c2e5aaf4723e35b
Terence <haha@example.com>
2023-02-05 15:15:36 +0800

    Update landing page img src path

--- development/frontend/src/pages/LandingPage.tsx
+++ development/frontend/src/pages/LandingPage.tsx
@@ -1,62 +1,35 @@
 import React, { useEffect, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Masonry from "react-masonry-css";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 // import Footer from "../components/Footer";
 // import Header from "../components/Header";
 import "./LandingPage.css";
 
-// 欠 tag handle
-const data1 = [
-  { id: 1, name: "1", img: "/uploads/1.jpg" },
-  { id: 2, name: "2", img: "/uploads/2.jpg" },
-  { id: 3, name: "3", img: "/uploads/3.jpg" },
-  { id: 4, name: "4", img: "/uploads/4.jpg" },
-  { id: 5, name: "5", img: "/uploads/5.jpg" },
-  { id: 6, name: "6", img: "/uploads/6.jpg" },
-  { id: 7, name: "7", img: "/uploads/7.jpg" },
-  { id: 8, name: "8", img: "/uploads/8.jpg" },
-  { id: 9, name: "9", img: "/uploads/9.jpg" },
-  { id: 10, name: "10", img: "/uploads/10.jpg" },
-  { id: 11, name: "11", img: "/uploads/11.jpg" },
-  { id: 12, name: "12", img: "/uploads/12.jpg" },
-  { id: 13, name: "13", img: "/uploads/13.jpg" },
-  { id: 14, name: "14", img: "/uploads/14.jpg" },
-  { id: 15, name: "15", img: "/uploads/15.jpg" },
-  { id: 16, name: "16", img: "/uploads/16.jpg" },
-  { id: 17, name: "17", img: "/uploads/17.jpg" },
-  { id: 18, name: "18", img: "/uploads/18.jpg" },
-  { id: 19, name: "19", img: "/uploads/19.jpg" },
-  { id: 20, name: "20", img: "/uploads/20.jpg" },
-  { id: 21, name: "21", img: "/uploads/21.jpg" },
-  { id: 22, name: "22", img: "/uploads/22.jpg" },
-  { id: 23, name: "23", img: "/uploads/23.jpg" },
-  { id: 24, name: "24", img: "/uploads/24.jpg" },
-  { id: 25, name: "25", img: "/uploads/25.jpg" },
-];
-
 export default function LandingPage() {
-  const [items, setItems] = useState<any>([]);
+  const [allPicture, setAllPicture] = useState<any>([]);
+  const [initialPicture, setInitialPicture] = useState<any>([]);
 
-  const [hasMore, sethasMore] = useState(true);
+  const [hasMore, setHasMore] = useState(true);
 
   const [indexA, setIndexA] = useState(8);
 
-  console.log("item no: ", data1.length - items.length);
-  console.log(hasMore);
-
   useEffect(() => {
-    const getComments = async () => {
-      setItems(data1.slice(0, 8));
-    };
+    async function getLanding() {
+      const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/user/landing`);
+      const json = await res.json();
 
-    getComments();
+      setAllPicture(json);
+      setInitialPicture(json.slice(0, 8));
+    }
+    getLanding();
   }, []);
 
   const fetchData = async () => {
-    setItems([...items, data1[indexA]]);
-    if (data1.length - items.length === 0 || data1.length - items.length < 3) {
-      sethasMore(false);
+    setInitialPicture([...initialPicture, allPicture[indexA]]);
+    if (allPicture.length - initialPicture.length < 2) {
+      setHasMore(false);
     }
     setIndexA(indexA + 1);
   };
@@ -67,6 +40,9 @@ export default function LandingPage() {
     290: 1,
   };
 
+  console.log("allPicture: ", allPicture);
+  console.log("item no: ", allPicture.length - initialPicture.length);
+  console.log(hasMore);
   return (
     <>
       <div className="landingPageContainer">
@@ -74,7 +50,7 @@ export default function LandingPage() {
 
         <div className="abc">
           <InfiniteScroll
-            dataLength={items.length}
+            dataLength={initialPicture.length}
             next={fetchData}
             hasMore={hasMore}
             loader={<h4>Loading...</h4>}
@@ -85,16 +61,18 @@ export default function LandingPage() {
             }
           >
             <Masonry breakpointCols={breakpointColumnsObj} className="my-masonry-grid" columnClassName="my-masonry-grid_column">
-              {items.map((item: any) => {
+              {initialPicture.map((item: any) => {
                 return (
-                  <div key={item.id}>
-                    <img src={`${item.img}`} alt={item.name} />
+                  <div key={item.name}>
+                    <img src={`${process.env.REACT_APP_S3_UPLOAD_URL}/${item.name}`} alt={item.name} />
                   </div>
                 );
               })}
             </Masonry>
           </InfiniteScroll>
         </div>
+
+        <Footer />
       </div>
     </>
   );

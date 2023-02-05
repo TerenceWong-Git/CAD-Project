319e77fb98b6a7e9b59e6eeeffdfadaa5a517f73
Terence <haha@example.com>
2023-02-05 15:16:15 +0800

    Update Header and Footer css

--- development/frontend/src/components/Footer.tsx
+++ development/frontend/src/components/Footer.tsx
@@ -1,35 +1,43 @@
+import { IconContext } from "react-icons";
+import { BiCommentDetail, BiHome } from "react-icons/bi";
+import { HiPlus } from "react-icons/hi2";
+import { FiMapPin } from "react-icons/fi";
+import { RiUserLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 export default function Footer() {
   return (
-    <div className="footer">Navbar</div>
+    <div className="footer">
+      <Link to={"/landing"} style={{ textDecoration: "none", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <IconContext.Provider value={{ style: { color: "black" } }}>
+          <BiHome />
+        </IconContext.Provider>
+      </Link>
 
-    // <div className="footerContainer">
-    //   <Button className="button" variant="contained" style={{ marginRight: "2vmin" }}>
-    //     <Link to={"/landing"} style={{ textDecoration: "none", display: "flex", justifyContent: "center", alignItems: "center" }}>
-    //       <BiHome />
-    //     </Link>
-    //   </Button>
-    //   <Button className="button" variant="contained" style={{ marginRight: "2vmin" }}>
-    //     <Link to={"/comments"} style={{ textDecoration: "none", display: "flex", justifyContent: "center", alignItems: "center" }}>
-    //       <BiCommentDetail />
-    //     </Link>
-    //   </Button>
-    //   <Button className="button" variant="contained" style={{ marginRight: "2vmin" }}>
-    //     <Link to={"/comments/createComment"} style={{ textDecoration: "none", display: "flex", justifyContent: "center", alignItems: "center" }}>
-    //       <HiPlus />
-    //     </Link>
-    //   </Button>
-    //   <Button className="button" variant="contained" style={{ marginRight: "2vmin" }}>
-    //     <Link to={"/map"} style={{ textDecoration: "none", display: "flex", justifyContent: "center", alignItems: "center" }}>
-    //       <FiMapPin />
-    //     </Link>
-    //   </Button>
-    //   <Button className="button" variant="contained">
-    //     <Link to={"/userprofile"} style={{ textDecoration: "none", display: "flex", justifyContent: "center", alignItems: "center" }}>
-    //       <RiUserLine />
-    //     </Link>
-    //   </Button>
-    // </div>
+      <Link to={"/comments"} style={{ textDecoration: "none", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <IconContext.Provider value={{ style: { color: "black" } }}>
+          <BiCommentDetail />
+        </IconContext.Provider>
+      </Link>
+
+      <Link to={"/comments/createComment"} style={{ textDecoration: "none", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <IconContext.Provider value={{ style: { color: "black" } }}>
+          <HiPlus />
+        </IconContext.Provider>
+      </Link>
+
+      <Link to={"/map"} style={{ textDecoration: "none", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <IconContext.Provider value={{ style: { color: "black" } }}>
+          <FiMapPin />
+        </IconContext.Provider>
+      </Link>
+
+      <Link to={"/userprofile"} style={{ textDecoration: "none", display: "flex", justifyContent: "center", alignItems: "center" }}>
+        <IconContext.Provider value={{ style: { color: "black" } }}>
+          <RiUserLine />
+        </IconContext.Provider>
+      </Link>
+    </div>
   );
 }
